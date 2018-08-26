1c36d545d371048266c35468de220ba71ef2d112
Eric <eric@ericyd.com>
2018-08-26 14:14:51 -0700

    site redesign - merge dev branch

--- assets/main.js
+++ assets/main.js
@@ -1,142 +1,47 @@
-// Polyfill Array.from, because I.E....
-// =====================================
-// Production steps of ECMA-262, Edition 6, 22.1.2.1
-if (!Array.from) {
-  Array.from = (function () {
-    var toStr = Object.prototype.toString;
-    var isCallable = function (fn) {
-      return typeof fn === 'function' || toStr.call(fn) === '[object Function]';
-    };
-    var toInteger = function (value) {
-      var number = Number(value);
-      if (isNaN(number)) { return 0; }
-      if (number === 0 || !isFinite(number)) { return number; }
-      return (number > 0 ? 1 : -1) * Math.floor(Math.abs(number));
-    };
-    var maxSafeInteger = Math.pow(2, 53) - 1;
-    var toLength = function (value) {
-      var len = toInteger(value);
-      return Math.min(Math.max(len, 0), maxSafeInteger);
-    };
-
-    // The length property of the from method is 1.
-    return function from(arrayLike/*, mapFn, thisArg */) {
-      // 1. Let C be the this value.
-      var C = this;
-
-      // 2. Let items be ToObject(arrayLike).
-      var items = Object(arrayLike);
-
-      // 3. ReturnIfAbrupt(items).
-      if (arrayLike == null) {
-        throw new TypeError('Array.from requires an array-like object - not null or undefined');
-      }
-
-      // 4. If mapfn is undefined, then let mapping be false.
-      var mapFn = arguments.length > 1 ? arguments[1] : void undefined;
-      var T;
-      if (typeof mapFn !== 'undefined') {
-        // 5. else
-        // 5. a If IsCallable(mapfn) is false, throw a TypeError exception.
-        if (!isCallable(mapFn)) {
-          throw new TypeError('Array.from: when provided, the second argument must be a function');
-        }
-
-        // 5. b. If thisArg was supplied, let T be thisArg; else let T be undefined.
-        if (arguments.length > 2) {
-          T = arguments[2];
-        }
-      }
-
-      // 10. Let lenValue be Get(items, "length").
-      // 11. Let len be ToLength(lenValue).
-      var len = toLength(items.length);
-
-      // 13. If IsConstructor(C) is true, then
-      // 13. a. Let A be the result of calling the [[Construct]] internal method 
-      // of C with an argument list containing the single item len.
-      // 14. a. Else, Let A be ArrayCreate(len).
-      var A = isCallable(C) ? Object(new C(len)) : new Array(len);
-
-      // 16. Let k be 0.
-      var k = 0;
-      // 17. Repeat, while k < len… (also steps a - h)
-      var kValue;
-      while (k < len) {
-        kValue = items[k];
-        if (mapFn) {
-          A[k] = typeof T === 'undefined' ? mapFn(kValue, k) : mapFn.call(T, kValue, k);
-        } else {
-          A[k] = kValue;
-        }
-        k += 1;
-      }
-      // 18. Let putStatus be Put(A, "length", len, true).
-      A.length = len;
-      // 20. Return A.
-      return A;
-    };
-  }());
-}
-
-
-
-
-
-// Actual code
-// =====================
-var contactLink = document.getElementById('contactlink');
-var contactEric = document.getElementById('contacteric');
-var emailSelect = document.getElementById('email-select');
-
-function showEmail() {
-  contactLink.style.display = 'none';
-  document.getElementById('email-holder').innerText = 'eric@ericyd.com';
-  contactEric.style.display = 'block';
-  contactEric.classList.add('active');
-  SelectText('email-holder');
-}
-
-function SelectText(id) {
-    var text = document.getElementById(id);
-    var range, selection;
-    if (document.body.createTextRange) {
-        range = document.body.createTextRange();
-        range.moveToElementText(text);
-        range.select();
-    } else if (window.getSelection) {
-        selection = window.getSelection();        
-        range = document.createRange();
-        range.selectNodeContents(text);
-        selection.removeAllRanges();
-        selection.addRange(range);
-    }
-}
-
-contactLink.addEventListener('click', showEmail);
-contactLink.addEventListener('keydown', showEmail);
-emailSelect.addEventListener('click', function(e) {
-  SelectText('email-holder');
-})
-
-function toggleMenu() {
-  var sidebar = document.querySelector('nav');
-  if (sidebar.classList.contains('collapsed')) {
-    sidebar.classList.remove('collapsed');
-    sidebar.classList.add('expanded');
-  } else {
-    sidebar.classList.remove('expanded');
-    sidebar.classList.add('collapsed');
+var forEachPolyfill = function (callback, thisArg) {
+  thisArg = thisArg || window;
+  for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
   }
+};
+
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = forEachPolyfill;
 }
+
+
+
+// function toggleMenu() {
+//   var sidebar = document.querySelector('nav');
+//   if (sidebar.classList.contains('collapsed')) {
+//     sidebar.classList.remove('collapsed');
+//     sidebar.classList.add('expanded');
+//   } else {
+//     sidebar.classList.remove('expanded');
+//     sidebar.classList.add('collapsed');
+//   }
+// }
 
 // maybe don't need to close it when links are clicked - either page will navigate or it will go external
 // document.querySelectorAll('.nav__link').forEach(function(element) {
 //   element.addEventListener('click', toggleMenu);
 // });
 
-// returns a NodeList
-// IE doesn't have Object.forEach method, so need to convert to arary
-Array.from(document.querySelectorAll('.toggle-menu')).forEach(function(element) {
-  element.addEventListener('click', toggleMenu);
+// document.querySelectorAll('.toggle-menu').forEach(function(element) {
+//   element.addEventListener('click', toggleMenu);
+// });
+
+
+// smooth scroll nav buttons
+document.querySelectorAll('.navigation__link').forEach(function(el) {
+  el.addEventListener('click', function(e) {
+    e.preventDefault();
+    var target = document.querySelector(e.target.dataset.scrollTo);
+    target.scrollIntoView({ 
+      behavior: 'smooth'
+    });
+    target.focus();
+  });
 });
+
+
--- index.md
+++ index.md
@@ -2,163 +2,27 @@
 # This comment is required for jekyll to process
 layout: home
 title: Home
+sections:
+- intro
+- profiles
+- projects
+- contact
 ---
 
-<span class="img--home">
-![Eric Dauenhauer]({{ 'assets/eric_dauenhauer.jpg' | relative_url }})
-</span>
+<!-- 
+<section class="section" id="intro">
+    My name is eric
+</section>
 
-#### An Ode to the Personal Website
+<section class="section" id="profiles">
+    These are my profiles
+</section>
 
-This website exists, I can be sure,
+<section class="section" id="projects">
 
-because you requested its
+</section>
 
-contents, and a server responded with
+<section class="section" id="contact">
+    <button id="contact">Show email address</button>
+</section> -->
 
-this unique sequence of bits.
-
-<br />
-
-But if I stop and ask myself
-
-"why is this site really here?"
-
-I cannot fathom the origin,
-
-except perhaps [too much beer?]({{ 'assets/eric_birthday_beer.jpg' | relative_url }})
-
-<br />
-
-It also may be possible
-
-this site is here because
-
-[Lifehacker tricked me](https://lifehacker.com/5958893/why-its-worth-it-to-purchase-your-own-domain-name) into believing
-
-owning a domain was all the buzz.
-
-<!-- <br />
-
-Yet the real answer may not be
-
-quite so clean and facile.
-
-There was a time I wanted a site
-
-~to practice web development.~ -->
-
-<br />
-
-My first rendition of this site
-
-was hosted by someone in flannel.
-
-Their service was superb and I got
-
-to play with my own cPanel!
-
-<br />
-
-But over time I found myself
-
-tinkering with the server less and less.
-
-It became clear that what I needed
-
-was a static site, not [express](https://www.npmjs.com/package/express).
-
-<br />
-
-I would also be remiss to omit
-
-the part this site once played,
-
-in shaping my online identity
-
-doing side projects, hoping to get paid.
-
-<br />
-
-But now I have a stable job and
-
-what once was hosted here
-
-has been moved to a wide range of profiles
-
-in the [inter]-[blog]-[web]-[sphere].
-
-[inter]: https://github.com/ericyd
-[blog]: https://bitbucket.org/ericyd/
-[web]: https://soundcloud.com/ericyd
-[sphere]: https://www.linkedin.com/in/ericdauenhauer/
-
-<!-- <br />
-
-As I make this shift to static site
-
-(hosted with love by Github)
-
-It makes me contemplate why at all
-
-~I keep the site online~ -->
-
-<br />
-
-I no longer want to host a blog,
-
-or code, or my resume,
-
-and I cannot help but think I may
-
-deprecate it all one day.
-
-<br />
-
-For the present, if you want,
-
-you can catch a (small) glimpse of the past.
-
-Checkout commit [4f8dc36](https://github.com/ericyd/ericyd.github.io/blob/4f8dc36550ec1d2c8e68a44071070fd74ca0fcd9/index.md)
-
-for some songs I like to blast.
-
-<br />
-
-I sometimes feel guilty for hosting
-
-a site that is just a collection of links,
-
-but then I check out Reddit
-
-and realize "yep, most of the internet stinks."
-
-<br />
-
-Though you may not know much more about me
-
-than when you started reading,
-
-I hope this poem offered something
-
-to break up the day's proceedings.
-
-<br />
-
-I wish I could say "come back soon!"
-
-with any sincerity,
-
-but the truth is (yep, you already guessed)
-
-updates here are a rarity.
-
-<br />
-
-So for now, I bid adieu,
-
-and leave you on your way.
-
-May the internet treat you well,
-
-if not always, at least today!
