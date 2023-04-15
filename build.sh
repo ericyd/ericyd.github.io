a31eea34a22da3387aec16f42379024e689d55a7
Eric Dauenhauer <eric@ericyd.com>
2023-04-15 17:35:38 -0500

    create build script and update publish script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+# really simple "build" script for now.
+# Mostly for finer control over what Wrangler publishes
+
+# stole from https://unix.stackexchange.com/questions/295033/loop-over-a-string-in-zsh-and-bash
+FILES_TO_COPY=(
+index.html
+style.css
+favicon.ico
+twitter
+twittier
+assets
+)
+
+mkdir -p build
+
+for filename in ${FILES_TO_COPY[@]}; do
+  cp -R $filename build
+done
